37aa3590e7e7eafc2920ebdc293a8946e88e76bf
R8bert <robertgeorge12012001@gmail.com>
2025-08-17 11:58:52 +0300

    commit

--- frontend/pingo/src/router/index.ts
+++ frontend/pingo/src/router/index.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useAuth } from '../composables/useAuth'
 import AccountPage from '../pages/account/AccountModern.vue'
 import HomePage from '../pages/main/Home.vue'
-import HomeWeTransferPage from '../pages/main/HomeWeTransfer.vue'
 import AuthPage from '../pages/auth/Auth.vue'
 
 const routes = [
@@ -10,12 +9,6 @@ const routes = [
     path: '/',
     name: 'Upload',
     component: HomePage,
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/wetransfer',
-    name: 'WeTransfer',
-    component: HomeWeTransferPage,
     meta: { requiresAuth: true }
   },
   // Download page route (public)
